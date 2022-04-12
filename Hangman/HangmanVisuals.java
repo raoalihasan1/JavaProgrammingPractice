7ccb70147631ef73698f217b3073828f7afa0535
Ali Hasan <alirao2880@gmail.com>
2022-04-12 15:43:27 +0000

    Hangman Visuals For Terminal

---
+++ Hangman/HangmanVisuals.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class HangmanVisuals {
+
+    private static String[] STAGES = new String[] {
+      "       \n       \n       \n       \n       \n       \n=========",
+      "       \n      |\n      |\n      |\n      |\n      |\n=========",
+      "  +---+\n      |\n      |\n      |\n      |\n      |\n=========",
+      "  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
+      "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
+      "  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
+      "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
+      "  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=========",
+      "  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=========",
+      "  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n=========",
+    };
+
+    public static String[] getEasyStages() {
+      String[] stages = new String[STAGES.length];
+      System.arraycopy(STAGES, 0, stages, 0, stages.length);
+      return stages;
+    }
+
+    public static String[] getHardStages() {
+      final int START_INDEX = 3;
+      String[] stages = new String[STAGES.length-START_INDEX];
+      System.arraycopy(STAGES, START_INDEX, stages, 0, stages.length);
+      return stages;
+    }
+
+    public static String getPicture(int currentState){
+      return(STAGES[9-currentState]);
+    }
+
+}
