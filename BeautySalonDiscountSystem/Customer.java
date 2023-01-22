c99646e4e4971504617164fc52458a1395508e9e
rao.hasan <rao.hasan@student.manchester.ac.uk>
2023-01-22 21:50:50 +0000

    Created A Package

---
+++ BeautySalonDiscountSystem/Customer.java
@@ -0,0 +1,33 @@
+package JavaProgrammingPractice.BeautySalonDiscountSystem;
+
+public class Customer {
+
+    private String Name;
+    private boolean Member;
+    private String memberType;
+
+    Customer(String customerName) {
+        Name = customerName;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public boolean isMember() {
+        return Member;
+    }
+
+    public void setMember(boolean areMember) {
+        Member = areMember;
+    }
+
+    public String getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(String typeOfMember) {
+        memberType = typeOfMember;
+    }
+
+}
---
+++ BeautySalonDiscountSystem/Visit.java
@@ -0,0 +1,94 @@
+package JavaProgrammingPractice.BeautySalonDiscountSystem;
+
+import java.text.DecimalFormat;
+
+public class Visit {
+
+    private Customer theCustomer;
+    public double serviceExpense;
+    public double productExpense;
+    private static final DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+    Visit(String customerName) {
+        theCustomer = new Customer(customerName);
+    }
+
+    public Customer getCustomer() {
+        return theCustomer;
+    }
+
+    public double getServiceExpense() {
+        return serviceExpense;
+    }
+
+    public void setServiceExpense(double ex) {
+        serviceExpense = ex;
+    }
+
+    public double getProductExpense() {
+        return productExpense;
+    }
+
+    public void setProductExpense(double ex) {
+        productExpense = ex;
+    }
+
+    public double getTotalExpense() {
+        if (theCustomer.isMember()) {
+            new DiscountRate();
+            double serviceDiscount = DiscountRate.getServiceDiscountRate(theCustomer.getMemberType());
+            double productDiscount = DiscountRate.getProductDiscountRate(theCustomer.getMemberType());
+            return ((1 - serviceDiscount) * serviceExpense + (1 - productDiscount) * productExpense);
+        } else {
+            return serviceExpense + productExpense;
+        }
+    }
+
+    public String toString(boolean isMember) {
+        if (isMember) {
+            return getCustomer().getMemberType() + " Member";
+        } else {
+            return "Non-Member";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Visit newCustomer = new Visit("Ali Hasan");
+        newCustomer.getCustomer().setMember(true);
+        newCustomer.getCustomer().setMemberType("Gold");
+        newCustomer.setServiceExpense(10.75);
+        newCustomer.setProductExpense(8.20);
+        System.out.println("--------------------\nCustomer Name: " + newCustomer.getCustomer().getName());
+        System.out.println("Membership Status: " + newCustomer.toString(newCustomer.getCustomer().isMember()));
+        System.out.println("Total Expense Is: $" + decimalFormat.format(newCustomer.getTotalExpense()));
+
+        Visit newCustomer2 = new Visit("Steve");
+        newCustomer2.setServiceExpense(10.75);
+        newCustomer2.setProductExpense(8.20);
+        System.out.println("--------------------\nCustomer Name: " + newCustomer2.getCustomer().getName());
+        System.out.println("Membership Status: " + newCustomer2.toString(newCustomer2.getCustomer().isMember()));
+        System.out.println("Total Expense Is: $" + decimalFormat.format(newCustomer2.getTotalExpense()));
+
+        Visit newCustomer3 = new Visit("Matthew");
+        newCustomer3.getCustomer().setMember(true);
+        newCustomer3.getCustomer().setMemberType("Silver");
+        newCustomer3.setServiceExpense(10.75);
+        newCustomer3.setProductExpense(8.20);
+        System.out.println("--------------------\nCustomer Name: " + newCustomer3.getCustomer().getName());
+        System.out.println("Membership Status: " + newCustomer3.toString(newCustomer3.getCustomer().isMember()));
+        System.out.println("Total Expense Is: $" + decimalFormat.format(newCustomer3.getTotalExpense()));
+
+        Visit newCustomer4 = new Visit("John");
+        newCustomer4.getCustomer().setMember(true);
+        newCustomer4.getCustomer().setMemberType("Premium");
+        newCustomer4.setServiceExpense(10.75);
+        newCustomer4.setProductExpense(8.20);
+        System.out.println("--------------------\nCustomer Name: " + newCustomer4.getCustomer().getName());
+        System.out.println("Membership Status: " + newCustomer4.toString(newCustomer4.getCustomer().isMember()));
+        System.out.println("Total Expense Is: $" + decimalFormat.format(newCustomer4.getTotalExpense())
+                + "\n--------------------");
+
+    }
+
+}
