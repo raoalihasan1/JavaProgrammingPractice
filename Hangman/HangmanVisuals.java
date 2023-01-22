961577170cf5552a996a0b5b21e5732d65d3ddf2
rao.hasan <rao.hasan@student.manchester.ac.uk>
2023-01-22 22:05:54 +0000

    Removed Variables/Imports Not Used

--- Hangman/HangmanVisuals.java
+++ Hangman/HangmanVisuals.java
@@ -1,8 +1,6 @@
-import java.util.Arrays;
-
 public class HangmanVisuals {
 
-    private static String[] STAGES = new String[] {
+  private static String[] STAGES = new String[] {
       "       \n       \n       \n       \n       \n       \n=========",
       "       \n      |\n      |\n      |\n      |\n      |\n=========",
       "  +---+\n      |\n      |\n      |\n      |\n      |\n=========",
@@ -13,23 +11,23 @@ public class HangmanVisuals {
       "  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=========",
       "  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=========",
       "  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n=========",
-    };
+  };
 
-    public static String[] getEasyStages() {
-      String[] stages = new String[STAGES.length];
-      System.arraycopy(STAGES, 0, stages, 0, stages.length);
-      return stages;
-    }
+  public static String[] getEasyStages() {
+    String[] stages = new String[STAGES.length];
+    System.arraycopy(STAGES, 0, stages, 0, stages.length);
+    return stages;
+  }
 
-    public static String[] getHardStages() {
-      final int START_INDEX = 3;
-      String[] stages = new String[STAGES.length-START_INDEX];
-      System.arraycopy(STAGES, START_INDEX, stages, 0, stages.length);
-      return stages;
-    }
+  public static String[] getHardStages() {
+    final int START_INDEX = 3;
+    String[] stages = new String[STAGES.length - START_INDEX];
+    System.arraycopy(STAGES, START_INDEX, stages, 0, stages.length);
+    return stages;
+  }
 
-    public static String getPicture(int currentState){
-      return(STAGES[9-currentState]);
-    }
+  public static String getPicture(int currentState) {
+    return (STAGES[9 - currentState]);
+  }
 
 }
